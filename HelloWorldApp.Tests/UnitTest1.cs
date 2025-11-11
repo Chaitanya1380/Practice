282c572619a194c7bdeb070f6fb25a6bba7ccce6
chaitanya <chaitanyasiripurapu2003@gmail.com>
2025-11-11 05:38:35 +0530

    Initial commit for Jenkins CI demo

---
+++ HelloWorldApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace HelloWorldApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
